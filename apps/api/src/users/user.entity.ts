69bebfe110a8be14b5319ca1da4fb1795dfae20e
malrang <seniya2@gmail.com>
2023-12-25 23:46:28 +0900

    feat: update

---
+++ apps/api/src/users/user.entity.ts
@@ -0,0 +1,37 @@
+import { Exclude } from 'class-transformer';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+// import { OneToMany } from 'typeorm';
+// import Post from '../posts/post.entity';
+
+@Entity()
+class User {
+  @PrimaryGeneratedColumn()
+  public id: number;
+
+  @Column({ unique: true })
+  public email: string;
+
+  @Column()
+  public name: string;
+
+  @Column()
+  @Exclude()
+  public password: string;
+
+  @Column({ default: '3' })
+  public lv: number;
+
+  @Column({ type: 'text', nullable: true })
+  public photo: string;
+
+  @Column({ type: 'boolean' })
+  public termsAgree: boolean;
+
+  @Column({ type: 'boolean', default: true })
+  public approval: boolean;
+
+  // @OneToMany(() => Post, (post: Post) => post.author)
+  // public posts?: Post[];
+}
+
+export default User;
