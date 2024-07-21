2bc42ec2efeef5c5994dd5fe6f40e1ef3e6ea792
malrang <seniya2@gmail.com>
2024-07-21 15:46:11 +0900

    feat: update

---
+++ home-nuxt/app.vue
@@ -0,0 +1,48 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'My App',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  },
+  script: [{ innerHTML: 'console.log(\'Hello world\')' }]
+})
+</script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+
+.rotate-enter-active,
+.rotate-leave-active {
+  transition: all 0.4s;
+}
+.rotate-enter-from,
+.rotate-leave-to {
+  opacity: 0;
+  transform: rotate3d(1, 1, 1, 15deg);
+}
+.layout-enter-active,
+.layout-leave-active {
+  transition: all 0.4s;
+}
+.layout-enter-from,
+.layout-leave-to {
+  filter: grayscale(1);
+}
+</style>
---
+++ home-nuxt/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>layout <slot /></div>
+</template>
+
+<script setup lang="ts">
+
+</script>
