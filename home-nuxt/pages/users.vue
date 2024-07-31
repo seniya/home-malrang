ef942580b5d9a0e6476b30a48bcc87782e5076a3
malrang <seniya2@gmail.com>
2024-07-31 22:25:19 +0900

    111

---
+++ home-nuxt/pages/users.vue
@@ -0,0 +1,6 @@
+<script setup lang="ts">
+const { data, error } = await useAsyncData('users', () => myGetFunction('users'))
+
+// This is also possible:
+// const { data, error } = await useAsyncData(() => myGetFunction('users'))
+</script>
